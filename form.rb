00475b7ec1d55d57358fb85003f9d55eb9c78ee6
Charles Castillo <charlescastillo1982@yahoo.com>
2015-10-02 17:26:12 -0400

    updated it all

--- form.rb
+++ form.rb
@@ -1,7 +1,33 @@
 require "sinatra"
+require "mandrill"
+
+m = Mandrill::API.new
+
 get "/home" do
 	erb :home
 end
 get "/cincinatti" do
 	"#{params}"
 end
+
+get "/philadelphia" do
+	if !params[:email].nil? &&
+	   !params[:name].nil? &&
+	   !params[:from_email].nil?
+
+		message = {
+			:subject=> "Hello from the Mandrill API",
+			:from_name=> "Your name",
+			:text=>"Word up?",
+			:to=>[{:email=> "#{params[:email]}",
+				:name=> "#{params[:name]}"}
+				],
+				:html=>"<html>I'm doing great, thanks for inquiring. Here's a random number for you: #{rand(100)}</html>",
+				:from_email=>"#{params[:from_email]}"
+			}
+			sending = m.messages.send(message)
+			"#{sending}"
+		else
+			"email=nil OR name=nil OR from_email=nil"
+		end
+	end
